0c9ca12fa89fb1b2972d3958c058c7bed9e56b2d
linhaiwebs <lhwebs@outlook.com>
2026-02-13 20:41:23 +0800

    Added .env.example

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,37 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  productionSourceMap: false,
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  outputDir: 'dist',
+  assetsDir: 'assets',
+
+  chainWebpack: (config) => {
+    config.optimization.splitChunks({
+      chunks: 'all',
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          priority: 10,
+        },
+        common: {
+          minChunks: 2,
+          priority: 5,
+          reuseExistingChunk: true,
+        },
+      },
+    });
+  },
+
+  configureWebpack: {
+    optimization: {
+      minimize: true,
+    },
+  },
+
+  devServer: {
+    port: 8080,
+    open: true,
+  },
 });
