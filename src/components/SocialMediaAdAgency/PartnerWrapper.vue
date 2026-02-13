6b21bd0cb31e88b11432e89848d89eee6e6e60bd
linhaiwebs <lhwebs@outlook.com>
2026-02-13 18:44:59 +0800

    Updated PartnerWrapper.vue

--- src/components/SocialMediaAdAgency/PartnerWrapper.vue
+++ src/components/SocialMediaAdAgency/PartnerWrapper.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="partner-wrapper">
-    <div class="partner-message-container">
-      <h3 class="partner-message">专一让我们更专业！</h3>
+    <div class="partner-logos-container">
+      <div
+        class="partner-logo-item"
+        v-for="platform in platforms"
+        :key="platform.id"
+        data-aos="fade-up"
+        data-aos-duration="1200"
+        :data-aos-delay="platform.delay"
+      >
+        <img :src="platform.logo" :alt="platform.name" />
+      </div>
     </div>
   </div>
 </template>
@@ -11,34 +20,76 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "PartnerWrapper",
+  data() {
+    return {
+      platforms: [
+        {
+          id: 1,
+          name: "Google",
+          logo: require("../../assets/images/icon-google-ads.png"),
+          delay: 100,
+        },
+        {
+          id: 2,
+          name: "Facebook",
+          logo: require("../../assets/images/icon-facebook.png"),
+          delay: 200,
+        },
+        {
+          id: 3,
+          name: "YouTube",
+          logo: require("../../assets/images/icon-youtube.png"),
+          delay: 300,
+        },
+        {
+          id: 4,
+          name: "Bing",
+          logo: require("../../assets/images/icon-microsoft-ads.png"),
+          delay: 400,
+        },
+        {
+          id: 5,
+          name: "Instagram",
+          logo: require("../../assets/images/icon-instagram.png"),
+          delay: 500,
+        },
+      ],
+    };
+  },
 });
 </script>
 
 <style lang="scss" scoped>
 .partner-wrapper {
-  padding: {
-    left: 110px;
-    right: 110px;
-    top: 70px;
-    bottom: 70px;
-  }
+  padding: 70px 110px;
 }
 
-.partner-message-container {
+.partner-logos-container {
   display: flex;
   align-items: center;
   justify-content: center;
-  min-height: 0;
+  gap: 60px;
+  flex-wrap: wrap;
 }
 
-.partner-message {
-  font-size: 32px;
-  font-weight: 700;
-  color: var(--headingColor);
-  margin: 0;
-  text-align: center;
-  font-family: var(--headingFontFamily);
-  letter-spacing: 2px;
+.partner-logo-item {
+  flex: 0 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img {
+    height: 60px;
+    width: auto;
+    object-fit: contain;
+    transition: transform 0.3s ease, opacity 0.3s ease;
+    opacity: 0.8;
+
+    &:hover {
+      transform: scale(1.1);
+      opacity: 1;
+    }
+  }
 }
 
 /* Max width 767px */
@@ -47,9 +98,14 @@ export default defineComponent({
     padding: 30px 20px;
   }
 
-  .partner-message {
-    font-size: 24px;
-    letter-spacing: 1px;
+  .partner-logos-container {
+    gap: 30px;
+  }
+
+  .partner-logo-item {
+    img {
+      height: 40px;
+    }
   }
 }
 
@@ -59,8 +115,14 @@ export default defineComponent({
     padding: 40px 20px;
   }
 
-  .partner-message {
-    font-size: 28px;
+  .partner-logos-container {
+    gap: 40px;
+  }
+
+  .partner-logo-item {
+    img {
+      height: 50px;
+    }
   }
 }
 
@@ -70,8 +132,14 @@ export default defineComponent({
     padding: 50px 30px;
   }
 
-  .partner-message {
-    font-size: 30px;
+  .partner-logos-container {
+    gap: 50px;
+  }
+
+  .partner-logo-item {
+    img {
+      height: 55px;
+    }
   }
 }
 
@@ -79,6 +147,10 @@ export default defineComponent({
 @media only screen and (min-width: 1200px) and (max-width: 1399px) {
   .partner-wrapper {
     padding: 60px 50px;
+  }
+
+  .partner-logos-container {
+    gap: 55px;
   }
 }
 </style>
